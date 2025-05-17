0d4d5a86465ac9ad1d087c5659e0ff0905ef1e14
zenglj <zenglj@live.com>
2025-05-17 18:51:02 +0800

    后端测试调整和增加

--- tools/riscv64-build-gdb.sh
+++ tools/riscv64-build-gdb.sh
@@ -9,13 +9,13 @@ fi
 "$1/build/minic" -S -A -o "$1/tests/$2.s" "$1/tests/$2.c"
 
 # 交叉编译程序成ARM32程序
-arm-linux-gnueabihf-gcc -g -static --include "$1/tests/std.h" -o "$1/tests/$2" "tests/$2.s" "$1/tests/std.c"
+riscv64-linux-gnu-gcc -g -static --include "$1/tests/std.h" -o "$1/tests/$2" "tests/$2.s" "$1/tests/std.c"
 
 echo "Now run gdb in another window"
 echo "Now gdb start"
 
 # 通过qemu运行，同时开启gdb server功能
-qemu-arm-static -g 1234 "$1/tests/$2"
+qemu-riscv64-static -g 1234 "$1/tests/$2"
 
 echo
 echo "Now gdb end"
---
+++ tools/riscv64-direct-gdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "arm32-direct-gdb.sh workspacefolder arm32filename"
+	exit 1
+fi
+
+# 交叉编译程序成ARM32程序
+riscv64-linux-gnu-gcc -g -static --include "$1/tests/std.h" -o "$1/tests/$2" "tests/$2.c" "$1/tests/std.c"
+
+echo "Now run gdb in another window"
+echo "Now gdb start"
+
+# 通过qemu运行，同时开启gdb server功能
+qemu-riscv64-static -g 1234 "$1/tests/$2"
+
+echo
+echo "Now gdb end"
+
+exit 0
